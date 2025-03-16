a8d76ac782049a034ffb1d9b772063334dcd5c2f
Churchill <churchillrimamskep@gmail.com>
2025-03-16 20:03:09 +0100

    connected to mongodb

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Todo struct {
@@ -14,58 +18,28 @@ type Todo struct {
 }
 
 func main(){
-	fmt.Println("Hello world")
-	app := fiber.New()
 
-	todos := []Todo{}
+	fmt.Println("hello world")
 
-	//without a database
-	app.Get("/api/todos",func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(todos)
-	})
+	err := godotenv.Load(".env")
+	if err != nil{
+		log.Fatal("Error loading .env file")
+	}
 
-	//create a todo
-	app.Post("/api/todos",func(c *fiber.Ctx) error{
-		todo := &Todo{}
+	MONGODB_URI := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+	client,err := mongo.Connect(context.Background(),clientOptions)
 
-		if err := c.BodyParser(todo); err != nil {
-			return err
-		}
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		if todo.Body == ""{
-			return c.Status(400).JSON(fiber.Map{"error":"Todo body is required"})
-		}
-		todo.ID = len(todos) + 1
-		todos = append(todos, *todo)
+	err = client.Ping(context.Background(),nil)
 
-		return c.Status(201).JSON(todo)
-	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	//Update a todo
-	app.Put("/api/todos/:id",func(c *fiber.Ctx) error {
-		id := c.Params("id")
+	fmt.Println("Connected to MONGODB ATLAS")
 
-		for i,todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				// todos[i].Completed = !todos[i].Completed
-				todos[i].Completed = true
-				return c.Status(200).JSON(todos[i])
-			}
-		}
-		return c.Status(404).JSON(fiber.Map{"error":"Todo is not found"})
-	})
-
-	//Delete a todo
-	app.Delete("/api/todos/:id",func(c *fiber.Ctx) error {
-		id := c.Params("id")
-
-		for i,todo := range todos {
-			if fmt.Sprint(todo.ID) == id {
-				todos = append(todos[:i],todos[i+1:]...) 
-				return c.Status(200).JSON(fiber.Map{"sucsess":"True"})
-			}
-		}
-		return c.Status(404).JSON(fiber.Map{"error":"Todo is not found"})
-	})
-	log.Fatal(app.Listen(":4000"))
 }
