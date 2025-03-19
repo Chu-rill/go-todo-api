0e7f3535c7b5cc77513c389b6ecdd098528e6976
Churchill <churchillrimamskep@gmail.com>
2025-03-19 18:56:54 +0100

    changed ui library to shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,25 @@
-import { Container, Stack } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
+// import { Container, Stack } from "@chakra-ui/react";
+// import Navbar from "./components/Navbar";
+// import TodoForm from "./components/TodoForm";
+// import TodoList from "./components/TodoList";
+
+import { Button } from "./components/ui/button";
 
 export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:4000/api" : "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
   return (
-    <Stack h="100vh">
-      <Navbar />
-      <Container>
-        <TodoForm />
-        <TodoList />
-      </Container>
-    </Stack>
+    // <Stack h="100vh">
+    //   <Navbar />
+    //   <Container>
+    //     <TodoForm />
+    //     <TodoList />
+    //   </Container>
+    // </Stack>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import theme from "./chakra/theme";
+
 import App from "./App.tsx";
+import "./index.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
+      <App />
     </QueryClientProvider>
   </StrictMode>
 );
